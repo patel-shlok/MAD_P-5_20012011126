b55174f3eb466818f5641da77476822314d776ab
patel-shlok <patelshlok11@gmail.com>
2022-10-01 11:22:01 +0530

    h

---
+++ app/src/main/java/com/example/shlok_pr5/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.shlok_pr5
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var playbtn : FloatingActionButton
+    private lateinit var stopbtn : FloatingActionButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        playbtn = findViewById(R.id.btn_play)
+        playbtn.setOnClickListener(View.OnClickListener{
+            play()
+        })
+        stopbtn = findViewById(R.id.btn_stop)
+        stopbtn.setOnClickListener(View.OnClickListener{
+            stop()
+        }) }
+    private fun play(){
+        Intent(applicationContext, MediaService::class.java).putExtra(MediaService.DATA_KEY,MediaService.DATA_VALUE).apply { startService(this) } }
+    private fun stop(){
+        Intent(applicationContext,MediaService::class.java).apply { stopService(this) }
+    }
+}
